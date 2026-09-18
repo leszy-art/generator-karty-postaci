e90523ea75528d3786a5aa6f605d0d0a62aa3b69
leszy-art <andraszak.a.paulina@gmail.com>
2026-09-18 22:39:39 +0200

    Update karty.py

--- karty.py
+++ karty.py
@@ -39,14 +39,14 @@ if st.button("Generuj postać"):
  elif płeć == "Mężczyzna":
   st.write("Imię:", wylosowany_początek + wylosowany_koniec_męski)
   st.write("Płeć: Mężczyzna")
-if st.button(  
- siła = random.randint(1,100)
- szybkość = random.randint(1,100)
- inteligencja = random.randint(1,100)
- charyzma = random.randint(1,100)
- szczęście = random.randint(1,100)
- zręczność = random.randint(1,100)
-)
+
+if st.button(siła = random.randint(1,100))
+if st.button(szybkość = random.randint(1,100))
+if st.button(inteligencja = random.randint(1,100))
+if st.button( charyzma = random.randint(1,100))
+if st.button( szczęście = random.randint(1,100))
+if st.button( zręczność = random.randint(1,100))
+
 klasy = [
   "Paladyn Kapitalista", 
   "Pół Majster - Pół Grafik", # łotrzyk,
