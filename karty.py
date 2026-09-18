a559d8b5a69b1b76f1ae576496f0457e96b2554c
leszy-art <andraszak.a.paulina@gmail.com>
2026-09-18 22:37:59 +0200

    Add random attribute generation for character stats

--- karty.py
+++ karty.py
@@ -39,13 +39,14 @@ if st.button("Generuj postać"):
  elif płeć == "Mężczyzna":
   st.write("Imię:", wylosowany_początek + wylosowany_koniec_męski)
   st.write("Płeć: Mężczyzna")
-  
+if st.button(  
 siła = random.randint(1,100)
 szybkość = random.randint(1,100)
 inteligencja = random.randint(1,100)
 charyzma = random.randint(1,100)
 szczęście = random.randint(1,100)
 zręczność = random.randint(1,100)
+)
 klasy = [
   "Paladyn Kapitalista", 
   "Pół Majster - Pół Grafik", # łotrzyk,
