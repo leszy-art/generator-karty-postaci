84662ed0984c86c3467367922b42fc25fb4023b2
leszy-art <andraszak.a.paulina@gmail.com>
2026-09-18 23:08:44 +0200

    Update karty.py

--- karty.py
+++ karty.py
@@ -47,39 +47,39 @@ if przycisk:
   charyzma = random.randint(1,100)
   szczęście = random.randint(1,100)
   zręczność = random.randint(1,100)
- if płeć == "Kobieta":
+  if płeć == "Kobieta":
    st.write("Imię:", wylosowany_początek + wylosowany_koniec_damski)
    st.write("Płeć: Kobieta")
- elif płeć == "Mężczyzna":
+  elif płeć == "Mężczyzna":
    st.write("Imię:", wylosowany_początek + wylosowany_koniec_męski)
    st.write("Płeć: Mężczyzna")
- klasy = [
+  klasy = [
    "Paladyn Kapitalista", 
    "Pół Majster - Pół Grafik", # łotrzyk,
    "Przeklęty Bard", 
    "Mnich Alkoholik" 
- ]
- wylosowana_klasa = random.choice(klasy)
- if wylosowana_klasa == "Paladyn Kapitalista":
-  siła = random.randint(81,100)
-  inteligencja = random.randint(61,100)
-  st.write("Klasa:", "Paladyn Kapitalista")
- elif wylosowana_klasa == "Pół Majster - Pół Grafik":
-  zręczność = random.randint(61,100)
-  charyzma = random.randint(61,100)
-  st.write("Klasa:", "Pół Majster - Pół Grafik")
- elif wylosowana_klasa == "Przeklęty Bard":
-  szczęście = random.randint(1,30)
-  charyzma = random.randint(61,100)
-  st.write("Klasa:", "Przeklęty Bard")
- elif wylosowana_klasa == "Mnich Alkoholik":
-  zręczność = random.randint(1,30)
-  szybkość = random.randint(1,30)
-  st.write("Klasa:", "Mnich Alkoholik")
-  st.write("STATYSTYKI:")
-  st.write("Siła:", siła)
-  st.write("Szybkość:", szybkość)
-  st.write("Zręczność:", zręczność)
-  st.write("Inteligencja:", inteligencja)
-  st.write("Charyzma:", charyzma)
-  st.write("Szczęście:", szczęście)
+  ]
+  wylosowana_klasa = random.choice(klasy)
+  if wylosowana_klasa == "Paladyn Kapitalista":
+   siła = random.randint(81,100)
+   inteligencja = random.randint(61,100)
+   st.write("Klasa:", "Paladyn Kapitalista")
+  elif wylosowana_klasa == "Pół Majster - Pół Grafik":
+   zręczność = random.randint(61,100)
+   charyzma = random.randint(61,100)
+   st.write("Klasa:", "Pół Majster - Pół Grafik")
+  elif wylosowana_klasa == "Przeklęty Bard":
+   szczęście = random.randint(1,30)
+   charyzma = random.randint(61,100)
+   st.write("Klasa:", "Przeklęty Bard")
+  elif wylosowana_klasa == "Mnich Alkoholik":
+   zręczność = random.randint(1,30)
+   szybkość = random.randint(1,30)
+   st.write("Klasa:", "Mnich Alkoholik")
+   st.write("STATYSTYKI:")
+   st.write("Siła:", siła)
+   st.write("Szybkość:", szybkość)
+   st.write("Zręczność:", zręczność)
+   st.write("Inteligencja:", inteligencja)
+   st.write("Charyzma:", charyzma)
+   st.write("Szczęście:", szczęście)
